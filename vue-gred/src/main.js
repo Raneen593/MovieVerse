3c58c5bbfc29f272b88a5c779c4d29fadf503e2b
Sama Mahmoud <ssamamahmoud23@gmail.com>
2026-09-02 15:35:41 +0300

    import boatstrap in main

--- vue-gred/src/main.js
+++ vue-gred/src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import router from "./router";
 import store from "./store";
-
+import './assets/index.css'
 createApp(App).use(store).use(router).mount("#app");
+
+
+
